f8fa44f1f029dd71d0bba8c1e172952667e0d68c
study-hary-id <haryansyah.study.id@gmail.com>
2021-08-04 15:14:31 +0700

    Update main.go Create server on 8000

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/study-hary-id/roman-numeral/romannumerals"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		urlPathElements := strings.Split(r.URL.Path, "/")
+
+		if urlPathElements[1] == "roman-numbers" {
+			number, _ := strconv.Atoi(strings.TrimSpace(urlPathElements[2]))
+
+			// Response failure because it's not in the data
+			if number == 0 || number > 10 {
+				w.WriteHeader(http.StatusNotFound)
+				w.Write([]byte("404 - Not Found"))
+				// Response success with response html text
+			} else {
+				fmt.Fprintf(w, "%q", html.EscapeString(romannumerals.Numerals[number]))
+			}
+
+			// Response failure if using random endpoints
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("400 - Bad Request"))
+		}
+	})
+
+	// Create a server and run it on 8000 port
+	s := &http.Server{
+		Addr:           ":8000",
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.ListenAndServe()
 }
