7b8db37c3709d66a6fe6c7c34d631aeb2f7b787c
study-hary-id <haryansyah.study.id@gmail.com>
2021-08-04 15:12:41 +0700

    Add Numerals roman data

--- romannumerals/data.go
+++ romannumerals/data.go
@@ -1,1 +1,14 @@
-package main
+package romannumerals
+
+var Numerals = map[int]string{
+	1:  "I",
+	2:  "II",
+	3:  "III",
+	4:  "IV",
+	5:  "V",
+	6:  "VI",
+	7:  "VII",
+	8:  "VIII",
+	9:  "IX",
+	10: "X",
+}
