f1d495224511eca88e3ca51515fe4c1b5d714c91
study-hary-id <haryansyah.study.id@gmail.com>
2021-08-30 13:07:04 +0700

    Refactor comments

--- main.go
+++ main.go
@@ -18,23 +18,23 @@ func main() {
 		if urlPathElements[1] == "roman-numbers" {
 			number, _ := strconv.Atoi(strings.TrimSpace(urlPathElements[2]))
 
-			// Response failure because it's not in the data
 			if number == 0 || number > 10 {
+				// Response failure because it's not in the data.
 				w.WriteHeader(http.StatusNotFound)
 				w.Write([]byte("404 - Not Found"))
-				// Response success with response html text
 			} else {
+				// Response success with response html text.
 				fmt.Fprintf(w, "%q", html.EscapeString(romannumerals.Numerals[number]))
 			}
 
-			// Response failure if using random endpoints
 		} else {
+			// Response failure if using random endpoints.
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte("400 - Bad Request"))
 		}
 	})
 
-	// Create a server and run it on 8000 port
+	// Create a server and run it on 8000 port.
 	s := &http.Server{
 		Addr:           ":8000",
 		ReadTimeout:    10 * time.Second,
