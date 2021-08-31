91686f55914b5a51583d9b2979628fab18e98786
study-hary-id <haryansyah.study.id@gmail.com>
2021-08-31 11:40:12 +0700

    Add 4 and 9 number patterns

--- romannumerals/data.go
+++ romannumerals/data.go
@@ -3,10 +3,16 @@ package romannumerals
 // Numerals is a group of data or static numbers in roman numerals.
 var Numerals = map[int]string{
 	1:    "I",
+	4:    "IV",
 	5:    "V",
+	9:    "IX",
 	10:   "X",
+	40:   "XL",
 	50:   "L",
+	90:   "XC",
 	100:  "C",
+	400:  "CD",
 	500:  "D",
+	900:  "CM",
 	1000: "M",
 }
