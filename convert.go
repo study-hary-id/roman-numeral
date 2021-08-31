140ead2adb49f60e97dd0c07dd56e9082a9bd386
study-hary-id <haryansyah.study.id@gmail.com>
2021-08-31 11:45:00 +0700

    Add algorithm to solve roman numerals

--- convert.go
+++ convert.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import "github.com/study-hary-id/roman-numeral/romannumerals"
 
-func convertToRoman(number int) string {
-	return fmt.Sprintf("%d -> roman number", number)
+// convertToRoman return roman numeral effectively until 3999.
+func convertToRoman(num int) (romanNum string) {
+
+	var (
+		keyNumerals = [13]int{1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000}
+		index       = len(keyNumerals) - 1
+	)
+	for num != 0 {
+		maxChance := num / keyNumerals[index]
+		num %= keyNumerals[index]
+
+		for maxChance != 0 {
+			romanNum += romannumerals.Numerals[keyNumerals[index]]
+			maxChance -= 1
+		}
+		index -= 1
+	}
+	return romanNum
+
 }
