87e89317b3fb9e6f78165379d61f3e3d6f053752
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-21 03:34:48 +0700

    Refactor HandleFunc main.go

--- main.go
+++ main.go
@@ -2,40 +2,28 @@ package main
 
 import (
 	"fmt"
+	"github.com/study-hary-id/roman-numeral-api/handlers"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
 	"time"
-
-	"github.com/study-hary-id/roman-numeral-api/handlers"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		urlPathElements := strings.Split(r.URL.Path, "/")
+		size := len(urlPathElements)
 
-		// Handle only roman-numbers endpoint.
-		if urlPathElements[1] == "roman-numbers" {
-			if len(urlPathElements) < 3 {
-				handlers.IncompleteRouteHandler(w)
-
-			} else {
-				num, err := strconv.Atoi(strings.TrimSpace(urlPathElements[2]))
-				if err != nil {
-					handlers.WrongParamHandler(w)
-				} else if num == 0 {
-					handlers.ZeroParamHandler(w)
-				} else {
-					handlers.RomanNumberHandler(w, num)
-				}
+		if size >= 3 {
+			num, _ := strconv.Atoi(strings.TrimSpace(urlPathElements[2]))
+			if num > 0 {
+				handlers.RomanNumber(w, r, num)
+				return
 			}
-
-		} else {
-			// Response failure if using random endpoints.
-			handlers.NotFound(w, r)
 		}
+		handlers.NotFound(w, r)
 	})
 
 	PORT := fmt.Sprintf(":%s", os.Getenv("PORT"))
