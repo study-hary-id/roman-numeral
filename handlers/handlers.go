9546f2d7e960c83284b84d0d685f81e9e9d7edad
study-hary-id <haryansyah.study.id@gmail.com>
2022-04-21 03:33:33 +0700

    Remove unnecessary handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -18,41 +18,11 @@ func NotFound(w http.ResponseWriter, r *http.Request) {
 	utils.ResponseError(w, http.StatusNotFound, errJSON)
 }
 
-// RomanNumberHandler handle roman-numbers endpoint with correct parameter.
-func RomanNumberHandler(w http.ResponseWriter, num int) {
+// RomanNumber handles roman-numbers endpoint with correct parameter.
+func RomanNumber(w http.ResponseWriter, _ *http.Request, num int) {
 	resJSON := models.Numeral{
 		Roman:   utils.ConvertToRoman(num),
 		Ordinal: num,
 	}
-	utils.ResponseJSON(w, http.StatusOK, models.Payload{Data: resJSON})
-}
-
-// IncompleteRouteHandler handle right URLs/routes/endpoints but missing parameter.
-func IncompleteRouteHandler(w http.ResponseWriter) {
-	errJSON := models.Errors{
-		Status: http.StatusBadRequest,
-		Title:  "Incomplete URL",
-		Detail: "/roman-numbers requires numeric parameter.",
-	}
-	utils.ResponseError(w, http.StatusBadRequest, errJSON)
-}
-
-// WrongParamHandler handler right URLs/routes/endpoints but wrong parameter.
-func WrongParamHandler(w http.ResponseWriter) {
-	errJSON := models.Errors{
-		Status: http.StatusBadRequest,
-		Title:  "Wrong Parameter",
-		Detail: "/roman-numbers requires numeric parameter.",
-	}
-	utils.ResponseError(w, http.StatusBadRequest, errJSON)
-}
-
-// ZeroParamHandler handle roman-numbers endpoint with number 0 parameter.
-func ZeroParamHandler(w http.ResponseWriter) {
-	errJSON := models.Errors{
-		Status: http.StatusNotFound,
-		Title:  "Number 0 Not Found",
-		Detail: "Roman numerals do not have the number 0.",
-	}
-	utils.ResponseError(w, http.StatusNotFound, errJSON)
+	utils.ResponseSuccess(w, http.StatusOK, resJSON)
 }
